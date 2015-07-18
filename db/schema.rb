104f919d5cfbbd171f28d5aff9c708cf52e79c14
Nayyir Jutha <nayyir.jutha@gmail.com>
2015-07-18 17:03:11 +0000

    url should now be a valid column in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718154720) do
+ActiveRecord::Schema.define(version: 20150718170132) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150718154720) do
     t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
 end
